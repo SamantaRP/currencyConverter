efaf9fe2e5fbc02185fc8eca6993589325f03343
SamantaRP <131348606+SamantaRP@users.noreply.github.com>
2023-04-20 11:01:35 +0200

    conversor euro libra and dolar
    conversor euro libra and dolar with index style and javascript file

---
+++ conversion.js
@@ -0,0 +1,71 @@
+
+var inputCantidad = document.getElementById("cantidad");
+var selectMonedaDe = document.getElementById("monedaDe");
+var selectMonedaA = document.getElementById("monedaA");
+
+// valor de todas las monedas
+// 1libra = 1.24 dolar
+let libradolar = 1.24;
+// 1e = 0.88 libras
+let eurolibra = 0.88;
+// 1e = 1.09 dolar
+let eurodolar = 1.09;
+
+
+function convertir(){
+let cantidad = inputCantidad.value;
+let monedaDe = selectMonedaDe.value;
+let monedaA = selectMonedaA.value;    
+let resultado = 0;
+if(monedaDe=="euro"){
+    switch(monedaA){
+        case'euro':
+        resultado = cantidad; 
+        break;
+        case'libra':
+        resultado = cantidad * eurolibra;
+        break;
+        case'dolar':
+        resultado = cantidad * eurodolar;
+        break;
+        default:
+            return;
+    } 
+}
+else if(monedaDe=="libra"){
+    switch(monedaA){
+        case'euro':
+        resultado = cantidad / eurolibra; 
+        break;
+        case'libra':
+        resultado = cantidad;
+        break;
+        case'dolar':
+        resultado = cantidad * libradolar;
+        break;
+        default:
+            return;
+    } 
+}
+
+else if(monedaDe=="dolar"){
+    switch(monedaA){
+        case'euro':
+        resultado = cantidad / eurodolar; 
+        break;
+        case'libra':
+        resultado = cantidad / libradolar;
+        break;
+        case'dolar':
+        resultado = cantidad;
+        break;
+        default:
+            return;
+    } 
+}
+else {
+    resultado = "error seleccione una moneda valida";
+}
+
+document.getElementById("resultado").innerHTML = resultado;
+}
